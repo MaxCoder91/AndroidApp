063c872248a32d75c0f0cbe7efa6f2f0c3c6c874
MaxCode <maxabarcadg@gmail.com>
2020-10-18 22:56:57 -0300

    Pruebas con integracion en bbdd

--- app/src/main/java/com/example/androidapp/MiCalendario.java
+++ app/src/main/java/com/example/androidapp/MiCalendario.java
@@ -2,13 +2,50 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MiCalendario extends AppCompatActivity {
-
+    ListView lvEventos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_calendario);
+
+        lvEventos=findViewById(R.id.lvEventos);
+
+        inflateView();
+    }
+
+    public ArrayList<String>ListarEventos(){
+        ArrayList<String>datos= new ArrayList<>();
+        DbHelper dbHelper = new DbHelper( this,"dbCheckp",null,1);
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        String query = "SELECT * FROM tblEvento";
+        Cursor cursor = sqLiteDatabase.rawQuery(query,null);
+        if(cursor.moveToFirst()){
+            do {
+                String titulo = cursor.getString(0);
+                String descripcion = cursor.getString(1);
+                String fecha = cursor.getString(2);
+                String hinicio = cursor.getString(3);
+                String hfinal = cursor.getString(4);
+                int idTipo = cursor.getInt(5);
+                int idCategoria = cursor.getInt(6);
+                String item = titulo+" "+descripcion+" "+fecha+" "+hinicio+" "+hfinal+" "+idTipo+" "+idCategoria;
+                datos.add(item);
+            }while (cursor.moveToNext());
+        }
+        return datos;
+    }
+    public void inflateView(){
+        ArrayList<String>origenDatos = ListarEventos();
+        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,origenDatos);
+        lvEventos.setAdapter(arrayAdapter);
     }
 }
