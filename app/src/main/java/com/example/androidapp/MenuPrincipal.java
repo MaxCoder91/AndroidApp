a69f3d6cd8471d93db9ea86bf5de8924f6bc7002
MaxCode <maxabarcadg@gmail.com>
2020-10-30 00:27:16 -0300

    Primera persistencia de datos

--- app/src/main/java/com/example/androidapp/MenuPrincipal.java
+++ app/src/main/java/com/example/androidapp/MenuPrincipal.java
@@ -28,7 +28,10 @@ public class MenuPrincipal extends AppCompatActivity {
         btnEventos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, Eventos.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
@@ -36,7 +39,10 @@ public class MenuPrincipal extends AppCompatActivity {
         btnTrabajo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, AgTrabajo.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
@@ -44,7 +50,10 @@ public class MenuPrincipal extends AppCompatActivity {
         btnEstudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, AgEstudios.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
@@ -52,7 +61,10 @@ public class MenuPrincipal extends AppCompatActivity {
         btnOtros.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, AgOtros.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
@@ -60,14 +72,20 @@ public class MenuPrincipal extends AppCompatActivity {
         tvCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, MainActivity.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
         MiCalendario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DaoUsuario user = new DaoUsuario();
+                user.setId(getIntent().getExtras().getInt("idUser"));
                 Intent intent = new Intent(MenuPrincipal.this, MiCalendario.class);
+                intent.putExtra("idUser",user.getId());
                 startActivity(intent);
             }
         });
